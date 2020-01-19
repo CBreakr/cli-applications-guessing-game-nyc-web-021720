02caeaa5a00bdabe3a45537906e421327252c010
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-19 04:02:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,35 @@
-# Code your solution here!
+
+#
+def run_guessing_game
+  
+end
+
+#
+def pick_number
+  
+end
+
+#
+def prompt_user
+  
+end
+
+#
+def get_user_guess
+  
+end
+
+#
+def evaluate_outcome(number, guess)
+  
+end
+
+#
+def user_success
+  
+end
+
+#
+def user_failure(number)
+  
+end
